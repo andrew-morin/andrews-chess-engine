80e98a4fe576aac87266be9d688d5a925380c6e2
Andrew Morin <andrew.morin@appian.com>
2021-11-01 22:59:05 -0400

    Add eslint and fix errors

--- www/index.js
+++ www/index.js
@@ -1,16 +1,16 @@
 import * as wasm from 'wasm-andrews-chess-engine';
-import black_pawn from './piece_images/black_pawn.svg';
-import black_bishop from './piece_images/black_bishop.svg';
-import black_knight from './piece_images/black_knight.svg';
-import black_rook from './piece_images/black_rook.svg';
-import black_queen from './piece_images/black_queen.svg';
-import black_king from './piece_images/black_king.svg';
-import white_pawn from './piece_images/white_pawn.svg';
-import white_bishop from './piece_images/white_bishop.svg';
-import white_knight from './piece_images/white_knight.svg';
-import white_rook from './piece_images/white_rook.svg';
-import white_queen from './piece_images/white_queen.svg';
-import white_king from './piece_images/white_king.svg';
+import blackPawn from './piece_images/black_pawn.svg';
+import blackBishop from './piece_images/black_bishop.svg';
+import blackKnight from './piece_images/black_knight.svg';
+import blackRook from './piece_images/black_rook.svg';
+import blackQueen from './piece_images/black_queen.svg';
+import blackKing from './piece_images/black_king.svg';
+import whitePawn from './piece_images/white_pawn.svg';
+import whiteBishop from './piece_images/white_bishop.svg';
+import whiteKnight from './piece_images/white_knight.svg';
+import whiteRook from './piece_images/white_rook.svg';
+import whiteQueen from './piece_images/white_queen.svg';
+import whiteKing from './piece_images/white_king.svg';
 
 const fen = wasm.print_board();
 
@@ -20,20 +20,17 @@ table.classList.add('board');
 const tbody = document.createElement('tbody');
 
 const fenRows = fen.split('/');
-fenRows.forEach(fenRow => {
+fenRows.forEach((fenRow) => {
   const row = document.createElement('tr');
 
-  for (let i = 0; i < fenRow.length; i++) {
+  for (let i = 0; i < fenRow.length; i += 1) {
     const char = fenRow.charAt(i);
     const int = parseInt(char, 10);
-    if (isNaN(int)) {
-      const cell = document.createElement('td');
-      const img = document.createElement('img');
-      img.src = getPieceImage(char);
-      cell.appendChild(img);
+    if (Number.isNaN(int)) {
+      const cell = getPieceCell(char);
       row.appendChild(cell);
     } else {
-      for (let j = 0; j < int; j++) {
+      for (let j = 0; j < int; j += 1) {
         const cell = document.createElement('td');
         row.appendChild(cell);
       }
@@ -47,18 +44,28 @@ table.appendChild(tbody);
 mainElement.appendChild(table);
 
 function getPieceImage(char) {
-  switch(char) {
-    case 'p': return black_pawn;
-    case 'b': return black_bishop;
-    case 'n': return black_knight;
-    case 'r': return black_rook;
-    case 'q': return black_queen;
-    case 'k': return black_king;
-    case 'P': return white_pawn;
-    case 'B': return white_bishop;
-    case 'N': return white_knight;
-    case 'R': return white_rook;
-    case 'Q': return white_queen;
-    case 'K': return white_king;
+  switch (char) {
+    case 'p': return blackPawn;
+    case 'b': return blackBishop;
+    case 'n': return blackKnight;
+    case 'r': return blackRook;
+    case 'q': return blackQueen;
+    case 'k': return blackKing;
+    case 'P': return whitePawn;
+    case 'B': return whiteBishop;
+    case 'N': return whiteKnight;
+    case 'R': return whiteRook;
+    case 'Q': return whiteQueen;
+    case 'K': return whiteKing;
+    default: return null;
   }
+}
+
+function getPieceCell(char) {
+  const cell = document.createElement('td');
+  const img = document.createElement('img');
+  img.src = getPieceImage(char);
+  cell.appendChild(img);
+
+  return cell;
 }
